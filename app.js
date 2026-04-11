c674bf3dc88c19c1c8348966ef8a37cd516d0751
solankitanu532-lgtm <solankitanu532@gamil.com>
2026-04-11 12:18:34 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -11,10 +11,11 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(userRouter)
 
-const PORT = process.env.DB_PORT 
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
